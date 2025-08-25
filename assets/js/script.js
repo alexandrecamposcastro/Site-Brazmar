f7193e9fc8854481668acfc882e6b48b532fc4f9
Alexandre <alek.campos07@gmail.com>
2025-08-25 11:32:35 -0300

    script que permite a alteração do horário estático para tempo real concluído

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,34 @@ jQuery(function ($) {
 });
 
 
+function updateClock() {
+    const timeZones = {
+        'time-sf' : 'America/Los_Angeles',
+        'time-ny' : 'America/New_York',
+        'time-br' : 'America/Fortaleza',
+        'time-ld' : 'Europe/London',
+        'time-sg' : 'Asia/Shanghai'
+    };
+
+    for (const [id, zone] of Object.entries(timeZones)) {
+        const now = new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12 : true,
+            timeZone : zone
+        });
+
+        const [time, ampm] = now.split(' ');
+        const el = document.getElementById(id);
+        if (el) {
+            el.innerHTML = `${time}<sub>${ampm}</sub>`;
+        }
+    }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
 
 
 
